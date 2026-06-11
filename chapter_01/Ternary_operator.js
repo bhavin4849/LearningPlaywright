539a72eb4d217f25b58d6d6ed772fb3ed41b4ee4
Bhavin Adit <bhavin4849@example.com>
2026-06-11 11:33:07 -0400

    Add chapter_01/Ternary_operator.js

---
+++ chapter_01/Ternary_operator.js
@@ -0,0 +1,4 @@
+let a = 10, b = 15, c = 6;
+let max = a > b ? (a > c ? "a is greater than" : "c is greatest") : (b > c ? "b is greatest" : "c is greatest");
+//let max = a > b ? "a is greatest" : "b is greatest";
+console.log(max);
