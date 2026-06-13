4bd5a1cf44c7f52911c0350e0b16c8b9c4e3d89f
Bhavin Adit <bhavin4849@example.com>
2026-06-13 08:39:16 -0400

    Add 03_variables.js

---
+++ chapter_01/03_variables.js
@@ -0,0 +1,8 @@
+let env = 'staging';
+const useriid = 1235;
+const apiurl = `https://api-${env}.example.com/user/${useriid}`;
+console.log(apiurl);
+
+let fname = 'John';
+let fullname = `Hi ${fname} Doe`;
+console.log(fullname);
