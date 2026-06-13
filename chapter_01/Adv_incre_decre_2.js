ccc7fd94e6c424eb3728aafe4b98e6399e44ce5a
Bhavin Adit <bhavin4849@example.com>
2026-06-13 08:36:52 -0400

    Add increment/decrement practice files

---
+++ chapter_01/Adv_incre_decre_2.js
@@ -0,0 +1,24 @@
+//let a = 10;
+//console.log(a++ + ++a); // 10
+
+
+
+/*
+A = a++ = 10 (first assign the value and then increment)
+a = 11 (after post increment)
++
+B = ++a = 12 (first increment and then assign)
+a = 12
+*/
+
+let a = 10;
+console.log(++a + ++a);
+console.log(a); // 12
+
+/*
+A = ++a = 11 (first assign the value and then increment)
+a = 11 (after pre increment)
++
+B = ++a = 12 (first increment and then assign)
+a = 12
+*/
---
+++ chapter_01/incre_decre.js
@@ -0,0 +1,5 @@
+let a = 10
+let b = ++a; // firt increment and then assign 
+console.log(a); // 11
+console.log(b); // 11
+
